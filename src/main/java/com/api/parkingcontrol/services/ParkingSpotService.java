55f8767bd93de36d5525dc536b20b9cc6d5c15af
Carlos Fonseca <carlosaacf@gmail.com>
2022-11-23 17:07:28 -0300

    Construcao dos metodos findAll e FindById

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -6,6 +6,10 @@ import lombok.Data;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Data
 @Service
 public class ParkingSpotService {
@@ -28,5 +32,13 @@ public class ParkingSpotService {
     }
     public boolean existsByApartmentAndBlock(String apartment, String block) {
         return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+
+    public List<ParkingSpotModel> findAll() {
+        return parkingSpotRepository.findAll();
+    }
+
+    public Optional<ParkingSpotModel> findAllById(UUID id) {
+        return parkingSpotRepository.findById(id);
     }
 }
